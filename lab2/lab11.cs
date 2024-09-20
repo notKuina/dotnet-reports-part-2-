35bc8476c5460e7da9184997741619110f770021
notKuina <kuinaa00@gmail.com>
2024-09-20 14:21:35 +0545

    Update lab11.cs

--- lab2/lab11.cs
+++ lab2/lab11.cs
@@ -2,30 +2,22 @@
 
 namespace GenericDemo
 {
-    // Generic class Box<T>
     public class Box<T>
     {
         private T _value;
-
-        // Constructor
         public Box(T value)
         {
             _value = value;
         }
-
-        // Method to set the value
         public void SetValue(T value)
         {
             _value = value;
         }
 
-        // Method to get the value
         public T GetValue()
         {
             return _value;
         }
-
-        // Method to display the value
         public void Display()
         {
             Console.WriteLine($"The value is: {_value}");
@@ -36,21 +28,17 @@ namespace GenericDemo
     {
         static void Main(string[] args)
         {
-            // Creating a Box instance for int
             Box<int> intBox = new Box<int>(123);
-            intBox.Display();  // Output: The value is: 123
+            intBox.Display();  
 
-            // Creating a Box instance for string
             Box<string> strBox = new Box<string>("Hello, Generics!");
-            strBox.Display();  // Output: The value is: Hello, Generics!
-
-            // Modifying the value in the integer box
+            strBox.Display(); 
+            
             intBox.SetValue(456);
-            intBox.Display();  // Output: The value is: 456
+            intBox.Display(); 
 
-            // Modifying the value in the string box
             strBox.SetValue("Generics in C#");
-            strBox.Display();  // Output: The value is: Generics in C#
+            strBox.Display(); 
 
             Console.ReadLine();
         }
